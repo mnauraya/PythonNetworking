db7bb22e778b6068ade856401e741fa91bcdec45
mnauraya <mnauraya@gmail.com>
2017-09-27 00:36:49 -0400

    Delete DeviceLibrary.py

--- DeviceLibrary.py
+++
@@ -1,3 +0,0 @@
-class DeviceLibrary():
-    router1 = {'Hostname': 'Router 1', 'IP Address':'192.168.1.127', 'OS': 'cisco_ios','Username':'Matt','Ln_Pass':'Lab', 'En_Pass':'lab'}
-    router2 = {'Hostname': 'Router 2', 'IP Address':'192.168.1.139', 'OS': 'cisco_ios','Username':'Matt','Ln_Pass':'Lab', 'En_Pass':'lab'}
